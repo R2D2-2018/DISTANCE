4d5f266d963ab8cac3d5ab662d2a97b9afc9ca69
Leidse <kietvanosnabrugge@ziggo.nl>
2018-05-13 13:29:05 +0200

    added functions conserning the peritybyte and the stopbyte of the uart protocol

--- src/UARTprotocol.cpp
+++ src/UARTprotocol.cpp
@@ -1,37 +1,56 @@
 #include "UARTprotocol.hpp"
 
-UARTprotocol::UARTprotocol(hwlib::target::pin_in RX1): 
-    RX1(RX1)
+UARTprotocol::UARTprotocol(hwlib::target::pin_in RX): 
+    RX(RX)
 {}
 
 int UARTprotocol::ReadByte() {
     unsigned int number = 0;
     for(int i = 0; i < 8; i++) {
         number = number << 1;
-        if (RX1.get()){
+        if (RX.get()){
             number++;
         }
-        //- eerste bit
     }
     return number;
 }
 
-bool UARTprotocol::WaitForStartByte() {
+void UARTprotocol::WaitForStartByte(int StartByte) {
     unsigned int number = 0;
-    while(!IsStartBit(number)){
+    while(!IsStartByte(StartByte, number)){
         number = number << 1;
-        if (RX1.get()){
+        if (RX.get()){
             number++;
         }
-        //- meest linker bit
+        if (number > 255){
+            if ((number % 2) == 0) {
+                number = number / 2;
+            } else {
+                number = number / 2;
+                number++;
+            }
+        }
     }
-    return true;
+    return;
 }
 
-bool UARTprotocol::IsStartBit(int number) {
-        if(number == 59){
-            return true;
-        } else {
-            return false;
-        }
+bool UARTprotocol::IsStartByte(int StartByte, int number) {
+    if(number == StartByte){
+        return true;
+            } else {
+        return false;
+    }
+}
+
+int UARTprotocol::ReadParityByte(){
+    return 50;
+}
+
+void UARTprotocol::WaitForStopByte(int stopByte){
+    hwlib::wait_ms(50);
+    return;
+}
+
+bool UARTprotocol::IsStopByte(int stopByte, int number){
+    return true;
 }
--- src/UARTprotocol.hpp
+++ src/UARTprotocol.hpp
@@ -2,16 +2,95 @@
 #define UARTprotocol_hpp
 #include "wrap-hwlib.hpp"
 
+/**
+ * @class UARTprotocol.h
+ * @author Wouter Dijkstra and Kiet van Osnabrugge
+ * @date 9-5-2018
+ * @file UARTprotocol.hpp
+ * @brief Class that can be used to use the UARTprotocol
+ */
 class UARTprotocol {
     private:
-        hwlib::target::pin_in RX1;
+        hwlib::target::pin_in RX;
     public:
-        UARTprotocol(hwlib::target::pin_in RX1);
+        UARTprotocol(hwlib::target::pin_in RX);
 
+        /**
+         * @brief 
+         * Returns a byte 
+         * 
+         * @description 
+         * Returns the byte that commes in form the RX port
+         * 
+         * @return 
+         * int number : a byte
+         */
         int ReadByte();
 
-        bool WaitForStartByte();
+        /**
+         * @brief 
+         * Waits for start byte
+         *
+         * @description 
+         * Waits till the startbyte commes in over the RX port
+         *
+         * @param
+         * Int StartByte : The start byte that needs to be waited for
+         */
+        void WaitForStartByte(int startByte);
 
-        bool IsStartBit(int number);
+        /**
+         * @brief Compares StartByte to a byte
+         * 
+         * @description 
+         * If the startByte is the same as Byte the function will retun true
+         *
+         * @param
+         * int StartByte : The StartByte that will be compaired with
+         * int number : The Byte that the StartByte will be compaired with
+         * 
+         * @return
+         * Bool : True or False
+         */
+        bool IsStartByte(int startByte, int number);
+
+        /**
+         * @brief 
+         * Returns the paritybyte;
+         * 
+         * @description 
+         * Returns the paritybyte that commes in form the RX port and knows if the sending of the data was correct
+         * 
+         * @return 
+         * int number : paritybyte
+         */
+        int ReadParityByte();
+
+        /**
+         * @brief 
+         * Waits for stop byte
+         *
+         * @description 
+         * Waits till the stopbyte commes in over the RX port
+         *
+         * @param
+         * Int StopByte : The stopbyte that needs to be waited for
+         */
+        void WaitForStopByte(int stopByte);
+
+        /**
+         * @brief Compares stopbyte to a byte
+         * 
+         * @description 
+         * If the stopByte is the same as Byte the function will retun true
+         *
+         * @param
+         * int stopByte : The stopByte that will be compaired with
+         * int number : The Byte that the StopByte will be compaired with
+         * 
+         * @return
+         * Bool : True or False
+         */
+        bool IsStopByte(int stopByte, int number);
 };
-#endif //x
+#endif //UARTprotocol
