636eb9b644f593e53411305d5a8ade608bd037e8
Leidse <kietvanosnabrugge@ziggo.nl>
2018-05-25 12:53:15 +0200

    Pushing some files I forgot like Sources.cmake and TestModule.cmake

--- BuildModule.cmake
+++ BuildModule.cmake
@@ -1,5 +1,14 @@
 include (../../flags.cmake)
 
+set (sources ${sources}
+    src/main.cpp
+    src/wrap-hwlib.cpp
+)
+
+add_definitions (-DBMPTK_TARGET_arduino_due
+                 -DBMPTK_TARGET=arduino_due
+                 -DBMPTK_BAUDRATE=19200)
+
 set (cxxflags
     "-Os"
     "-ffunction-sections"
